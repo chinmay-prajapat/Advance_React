5474cb76d65c640e53cc9764e97eff8459eddba1
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 11:27:55 +0530

    reset/decrement

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,14 +5,26 @@ const UseStateCounter = () => {
   const increment = () => {
     setValue(value + 1);
   };
-
+  const decrement = () => {
+    if (value === 0) {
+      return null;
+    }
+    setValue(value - 1);
+  };
+  const reset = () => {
+    setValue(0);
+  };
   return (
     <div>
       <section style={{ margin: "4rem 0" }}>
         <h2>Regular Counter</h2>
         <h1>{value}</h1>
-        <button className="btn">Decrease</button>
-        <button className="btn">reset</button>
+        <button className="btn" onClick={decrement}>
+          Decrease
+        </button>
+        <button className="btn" onClick={reset}>
+          reset
+        </button>
         <button className="btn" onClick={increment}>
           increase
         </button>
