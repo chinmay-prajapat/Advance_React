3c35d9e7b3d3c23dd326985030d69168517868c1
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 16:24:56 +0530

    useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -4,9 +4,13 @@ import React, { useState, useEffect } from "react";
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   useEffect(() => {
     console.log("call useEffect");
-  });
+    if (value >= 1) {
+      document.title = `New Message(${value})`;
+    }
+  }, [value]);
   console.log("render component");
   return (
     <div>
