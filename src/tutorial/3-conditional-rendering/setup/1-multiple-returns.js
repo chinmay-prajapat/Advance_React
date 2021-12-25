79f15ff63b031338859310d2aa6e7b308ab40ff5
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 17:32:46 +0530

    conditional loading

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,39 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [user, setUser] = useState("default user");
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+        console.log(user);
+      })
+      .catch((error) => console.log("error"));
+  }, []);
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
