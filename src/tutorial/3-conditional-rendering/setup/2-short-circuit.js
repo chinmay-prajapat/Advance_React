3e4ed90d06ab50b234b6e94f9b5f08db0029cbaa
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 21:14:53 +0530

    forms

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hellow world";
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <div>
+      <h1>{firstValue}</h1>
+      <h1>value:{secondValue}</h1>
+    </div>
+  );
 };
 
 export default ShortCircuit;
