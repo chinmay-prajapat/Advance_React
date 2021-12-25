36bfe01502f390afdfef8a3f4859fad0be24230d
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 08:42:59 +0530

    removing array item

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    setPeople(people.filter((item) => item.id !== id));
+  };
+  return (
+    <div>
+      <h2>
+        {people.map((item) => {
+          const { id, name } = item;
+          return (
+            <div key={id} className="item">
+              {name}
+              <button onClick={() => removeItem(id)}>Remove</button>
+            </div>
+          );
+        })}
+      </h2>
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
