314fa4fd935a542d3435840cf17ca2bdf17876b7
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 17:41:44 +0530

    error loading

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://api.github.com/users/QuincyLarsons";
 const MultipleReturns = () => {
   const [isLoading, setLoading] = useState(true);
   const [isError, setError] = useState(false);
   const [user, setUser] = useState("default user");
   useEffect(() => {
+    fetch(url).then((res) => {
+      if (res.status >= 200 && res.status <= 299) {
+        return res.json();
+      } else {
+        setLoading(false);
+        setError(true);
+        throw new Error(res.statusText);
+      }
+    });
     fetch(url)
       .then((res) => res.json())
       .then((user) => {
