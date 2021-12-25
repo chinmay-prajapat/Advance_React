37b161f52e76a471ac89c1b17f4cecc085319f7e
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-25 08:33:09 +0530

    useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("random title");
+
+  const handleClick = () => {
+    if (title === "random title") {
+      setTitle("certainly");
+    } else {
+      setTitle("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
